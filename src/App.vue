9dcbf1b7be299c6c1fedc7bac6d02fff5ee05d04
mahmoudMagdy1255 <mahmoud.magdy1255@gmail.com>
2023-05-21 23:01:17 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.pngaaaa">
   <HelloWorld v-model="text" v-model:last-value="lastValue" />
   <UserComponent />
 </template>
